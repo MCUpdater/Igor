55fe19a3e2f22bb356feb52e97b708a333e40713
Ammon Lauritzen <alauritzen@aspyr.com>
2014-10-06 12:31:12 -0500

    Compiles against MC1.7 now.

--- src/main/java/org/mcupdater/igor/Igor.java
+++ src/main/java/org/mcupdater/igor/Igor.java
@@ -2,18 +2,16 @@ package org.mcupdater.igor;
 
 import java.util.logging.Logger;
 
-import net.minecraftforge.common.Configuration;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.config.Configuration;
 
 import org.mcupdater.igor.daemon.Daemon;
 
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import cpw.mods.fml.common.network.NetworkMod;
 
 @Mod(modid = Version.MOD_ID, name = Version.MOD_NAME, version = Version.VERSION, acceptedMinecraftVersions = "[1.6,1.7],", dependencies = "")
-@NetworkMod(clientSideRequired = false, serverSideRequired = true)
 public class Igor {
 	public static Configuration config;
 	public static Logger		log	= Logger.getLogger(Version.MOD_NAME);
